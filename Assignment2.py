815da45c6daca8034d6d281ed5f81a5f37a9de6c
apadman2 <70728634+apadman2@users.noreply.github.com>
2020-10-06 12:38:31 -0400

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,13 @@
+import requests as req
+
+for j in range(50):
+    response = req.get("http://3.95.249.159:8000/random_company")
+    total = response.text
+    sentence = total.split("\n")
+    for i in sentence:
+        if 'Name: ' in i:
+            n = i[16:-5]
+            print("({0}){1}".format(j+1, n))
+        if 'Purpose: ' in i:
+            p = i[16:-5]
+            print("({0}){1}".format(j+1, p))
